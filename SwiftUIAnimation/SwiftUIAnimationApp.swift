a9dc0d38f950e82b944241e61aaeb03eba90b612
Leonardo Fernandes <leonardo.gomes@lit.ifce.edu.br>
2022-10-03 18:07:17 -0300

    Initial Commit

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by Leonardo Gomes Fernandes on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
